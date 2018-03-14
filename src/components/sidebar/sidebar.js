34e417080139fe151d53e35752b1a350415a5c3a
ivan minutillo <minutillo.ivan@gmail.com>
2018-03-14 23:57:46 +0100

    sidebar UI refactor

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -4,6 +4,7 @@ import {Left, Right, User, Preferites, Settings, Power, More} from '../../icons'
 import {NavLink, withRouter} from 'react-router-dom'
 
 const Sidebar = (props) => {
+  console.log(props)
   let result = props.agents.reduce(function (r, a) {
     let type = a.relationship.label.split(' ', 3)[1]
     r[type] = r[type] || []
@@ -43,14 +44,22 @@ const Sidebar = (props) => {
     <div className={style.sidebar}>
       <header className={style.sidebar_header}>
       <div className={style.header_menu}>
-        <div className={style.sidebar_navigation}>
+        {/* <div className={style.sidebar_navigation}>
           <span onClick={() => props.history.goBack()}><Left width={18} height={18} color={'#fff'} /></span>
           <span onClick={() => props.history.goForward()}><Right width={18} height={18} color={'#fff'} /></span>
+        </div> */}
+        {/* <NavLink exact className={style.home_button} activeClassName={style.activeLink} to={'/'}>Dashboard</NavLink> */}
+        <div className={style.header_profile}>
+          <NavLink to={'/agent/' + props.data.id}>
+            <div className={style.data_image}>
+              <img src={props.data.image} />
+            </div>
+            <h3>{props.data.name}</h3>
+          </NavLink>
         </div>
-        <NavLink exact className={style.home_button} activeClassName={style.activeLink} to={'/'}>Dashboard</NavLink>
         <div className={style.right_profile}>
           {/* <span><Bell width={'16'} height={'16'} color='#fff' /></span> */}
-          <span onClick={props.handleToggleProfilePanel}><More onClick={props.handleToggleProfilePanel} color='#fff' /></span>
+          <span onClick={props.handleToggleProfilePanel}><More onClick={props.handleToggleProfilePanel} color='#fff' width='18' height='18' /></span>
         </div>
         <div className={props.profile ? style.right_profile_panel + ' ' + style.profileActive : style.right_profile_panel }>
           <div className={style.panel_section}>
@@ -60,34 +69,38 @@ const Sidebar = (props) => {
           <button onClick={props.logout} className={style.logout}>Logout</button>
         </div>
         </div>
-        <div className={style.header_profile}>
+        {/* <div className={style.header_profile}>
           <NavLink to={'/agent/' + props.data.id}>
             <div className={style.data_image}>
               <img src={props.data.image} />
             </div>
             <h3>{props.data.name}</h3>
           </NavLink>
-        </div>
+        </div> */}
       </header>
       <section className={style.sidebar_section}>
         <h3 className={style.section_title}>Apps</h3>
         <ul className={style.section_list}>
           <li className={style.list_active}><NavLink to={'/'}>Dashboard</NavLink></li>
-          <li><a href="https://board.net/p/Fair_Health_Care" target='blank'>Health</a></li>
+          <li className={style.list_active}><NavLink to={'/agent/' + props.data.id}>Work</NavLink></li>
+          <li><a href="https://board.net/p/Fair_Health_Care_v4" target='blank'>Health</a></li>
           <li><a target='blank' href="https://wallet.bankofthecommons.coop">Wallet</a></li>
         </ul>
       </section>
       <section className={style.aside_section}>
+      <h3 className={style.section_title}>Network</h3>
       {Object.keys(result).map((item, i) => (
         <div key={i}>
-          <h3 className={style.section_title}>{Object.keys(result)[i]}</h3>
           <ul className={style.section_list}>
             {result[Object.keys(result)[i]].map((agent, i) => (
               <NavLink key={i} activeClassName={style.activeLink} to={'/agent/' + agent.object.id}>
                 <li>
                   <div className={style.list_agent}>
                     <img className={style.agent_photo} src="https://picsum.photos/200/300" />
-                    <h4>{agent.object.name}</h4>
+                    <div className={style.agent_label}>
+                      <h4>{agent.object.name}</h4>
+                      <h6>{agent.relationship.label.split(' ', 3)[1]}</h6>
+                    </div>
                   </div>
                 </li>
               </NavLink>
